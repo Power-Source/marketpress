401272bf6d72f245a82dd978b4e4af85d98bd795
DerN3rd <dern3rd@outlook.at>
2026-04-07 18:07:25 +0200

    Update: Erhöhe die Versionsnummer auf 1.0.7 in der Hauptdatei und der readme.txt

--- marketpress.php
+++ marketpress.php
@@ -3,7 +3,7 @@
 Plugin Name: PS MarketPress
 Plugin URI:  https://psource.eimen.net/wiki/ps-marketpress-dokumentation/
 Description: Das komplette ClassicPress E-Commerce-Plugin – funktioniert auch perfekt mit Multisite, um einen sozialen Marktplatz zu erstellen, auf dem Du eine Provision einbehalten kannst! Aktiviere das Plugin, passen Deine Einstellungen an und füge dann Produkte zu Deinem Shop hinzu.
-Version:     1.0.6
+Version:     1.0.7
 Author:      PSOURCE
 Author URI:  https://psource.eimen.net/
 Text Domain: mp
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-define( 'MP_VERSION', '1.0.6' );
+define( 'MP_VERSION', '1.0.7' );
 
 /**
  * Main class Marketpress.
@@ -105,7 +105,7 @@ class Marketpress {
 	 * @since 1.0.1
 	 * @var string
 	 */
-	public $version = null;
+	public $version = MP_VERSION;
 
 	/**
 	 * Gets the single instance of the class
